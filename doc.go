a2735c0c90fa850c76fadc2e640980485ee50491
seong <kang.seongmin@gmail.com>
2014-07-12 20:05:43 +0200

    first doc, starting with flag parsing

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+yxorp - a tiny reverse-proxy
+
+Installation:
+    go get github.com/gomicroprojects/yxorp
+
+This is the package doc. It can be placed in a doc.go file, which is a convention.
+
+See http://blog.golang.org/godoc-documenting-go-code
+*/
+package main
---
+++ yxorp.go
@@ -0,0 +1,42 @@
+package main
+
+// package main
+//
+// The package clause (http://golang.org/ref/spec#Package_clause)
+// "main" indicates that this file is a command and will generate an executable
+// binary.
+//
+// Take a look at http://golang.org/doc/code.html for an introduction on
+// "How to Write Go Code"
+
+// The import declaration (http://golang.org/ref/spec#Import_declarations)
+// This is in the multiline format.
+// You could also write:
+//     import a
+//     import b
+import (
+	// The flag package (http://golang.org/pkg/flag/)
+	// We will us this to parse command line flags
+	"flag"
+)
+
+// We declare a variable "configFileName", which is a string
+// Since it is just declared, it is inialized to its "zero value".
+// The zero value for a string is an empty string ""
+var configFileName string
+
+func main() {
+	// Setting up the flags using the flag package
+	//
+	// The StringVar function takes a pointer to a string. Since the flag package will modify the contents of the
+	// "configFileName" string for us, it needs to know the address of the string. Simply passing the value of the
+	// (now empty) string would not be enough.
+	// To get the pointer to our "configFileName" var, we take the address of it with the ampersand operator "&".
+	//
+	// Our flag name will be "c", the default value an empty string, and a short description.
+	flag.StringVar(&configFileName, "c", "", "The config file name to use. Example: /tmp/yxorp.json")
+	// flag.Parse() will parse the flags and do its magic
+	//
+	// As an added bonus we have a basic help message with the -h flag built-in. Try it out.
+	flag.Parse()
+}
